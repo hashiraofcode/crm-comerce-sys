31a287a4e48e825d060ceb504384ef3ca725786e
Valmir-Neto <netovalmirr@gmail.com>
2025-08-18 19:00:22 -0300

    Feat: install material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+// Páginas
+import { Home } from '@/pages/index.ts'
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<h1>Criando rotas</h1>} />
+      <Route path="/pagina-inicial" element={<Home />} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
