9f65b939004a4946d51aea86c272a314e3262412
Valmir-Neto <netovalmirr@gmail.com>
2025-08-18 20:34:54 -0300

    Feat: create themes for application

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@mui/material'
+import { lightTheme } from './theme/index.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
