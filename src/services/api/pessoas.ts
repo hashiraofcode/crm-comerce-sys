0a68dcf2f386d61f4a8f5b6f2aeb8a3b5ff2c425
Valmir-Neto <netovalmirr@gmail.com>
2025-09-20 11:05:01 -0300

    Feat: too much api's consumers configured

--- src/services/api/pessoas.ts
+++ src/services/api/pessoas.ts
@@ -42,6 +42,7 @@ const getById = async (
 ): Promise<detailsPeople | Error> => {
   try {
     const data = await apiInstance({
+      method: 'GET',
       url: `/pessoas/${id}`,
       headers: {
         ...configs?.headers,
@@ -62,8 +63,88 @@ const getById = async (
     )
   }
 }
-const create = async (): Promise<any> => {}
-const upDateById = async (): Promise<any> => {}
-const deletById = async (): Promise<any> => {}
+const create = async (
+  person: Omit<detailsPeople, 'id'>,
+  configs?: AxiosRequestConfig,
+): Promise<number | Error> => {
+  try {
+    const data = await apiInstance({
+      method: 'GET',
+      url: `/pessoas`,
+      data: person,
+      headers: {
+        ...configs?.headers,
+      },
+      ...configs,
+    })
+    const handelerId: detailsPeople = data.data
+    if (data) {
+      return handelerId.id
+    }
+    throw new Error('Erro de busca pelo id')
+  } catch (e) {
+    console.error(
+      'Error: ' + (e as { mensage: string }).mensage ||
+        'erro no registro do cliente',
+    )
+    return new Error(
+      (e as { mensage?: string }).mensage || 'erro no registro do cliente',
+    )
+  }
+}
+const upDateById = async (
+  person: detailsPeople,
+  configs?: AxiosRequestConfig,
+): Promise<void | Error> => {
+  try {
+    await apiInstance({
+      method: 'PUT',
+      url: `/pessoas/${person.id}`,
+      data: person,
+      headers: {
+        ...configs?.headers,
+      },
+      ...configs,
+    })
+
+    throw new Error('Erro na atualização de cadastro do cliente')
+  } catch (e) {
+    console.error(
+      'Error: ' + (e as { mensage: string }).mensage ||
+        'erro no registro do cliente',
+    )
+    return new Error(
+      (e as { mensage?: string }).mensage ||
+        'Erro na atualização de cadastro do cliente',
+    )
+  }
+}
+const deletById = async (
+  person: detailsPeople,
+  configs?: AxiosRequestConfig,
+): Promise<any> => {
+  try {
+    const data = await apiInstance({
+      method: 'DELETE',
+      url: `/pessoas/${person.id}`,
+      headers: {
+        ...configs?.headers,
+      },
+      ...configs,
+    })
+    if (data.status !== 200) {
+      throw new Error('Erro na atualização de cadastro do cliente')
+    }
+  } catch (e) {
+    console.error(
+      'Error: ' + (e as { mensage: string }).mensage ||
+        'Erro na deleção do cadastro',
+    )
+    return new Error(
+      (e as { mensage?: string }).mensage ||
+        'Erro na atualização de cadastro do cliente',
+    )
+  }
+}
 
 export { getAll, getById, create, upDateById, deletById }
